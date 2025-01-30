4261ab8f80a9d69cd2e109085a939b83dd077281
Lostovayne <moises.barrera.dev@gmail.com>
2025-01-30 16:42:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+const App = () => {
+	return (
+		<div className="content">
+			<h1>Rsbuild with React</h1>
+			<p>Start building amazing things with Rsbuild.</p>
+			<div className="max-w-md h-auto">
+				<video
+					src="https://ik.imagekit.io/deus/xlargex.mp4?tr=w-500"
+					loop
+					muted
+					autoPlay
+					poster="https://ik.imagekit.io/deus/xlargex.mp4/ik-thumbnail.jpg?tr=so-2"
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default App;
